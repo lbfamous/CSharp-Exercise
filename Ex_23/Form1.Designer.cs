fb46e0470d8e24d9cd22480cba3c4f47e42984c4
lbfamous <57867574@qq.com>
2015-05-16 02:20:49 +0800

    1. Add Exercise 23 -- A Button can not click

---
+++ Ex_23/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Ex_23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_catchMe = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_catchMe
+            // 
+            this.btn_catchMe.Location = new System.Drawing.Point(100, 100);
+            this.btn_catchMe.Name = "btn_catchMe";
+            this.btn_catchMe.Size = new System.Drawing.Size(75, 23);
+            this.btn_catchMe.TabIndex = 0;
+            this.btn_catchMe.Text = "来抓我呀";
+            this.btn_catchMe.UseVisualStyleBackColor = true;
+            this.btn_catchMe.Click += new System.EventHandler(this.btn_catchMe_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btn_catchMe);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_catchMe;
+    }
+}
+
---
+++ Ex_23/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex_23
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_catchMe_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("抓到我了，算你聪明", "抓到了");
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int border = 50;
+            int x = e.X;
+            int y = e.Y;
+            int left = btn_catchMe.Left;
+            int right = btn_catchMe.Right;
+            int top = btn_catchMe.Top;
+            int bottom = btn_catchMe.Bottom;
+		    if(x > left-border && x <right + border && y > top-border && y <bottom+border)
+            {
+                btn_catchMe.Top += (y > top ? -20 : 20);
+			    if(btn_catchMe.Top > Form1.ActiveForm.Size.Height || btn_catchMe.Bottom <0)
+                {
+                    btn_catchMe.Top = Form1.ActiveForm.Size.Height / 2;
+                }
+                btn_catchMe.Left += (x > left ? -20 : 20);
+                if(btn_catchMe.Left > Form1.ActiveForm.Size.Width || btn_catchMe.Right <0)
+                {
+                    btn_catchMe.Left = Form1.ActiveForm.Size.Width / 2;
+                }
+            }
+		
+		
+		
+        }
+
+    }
+}
